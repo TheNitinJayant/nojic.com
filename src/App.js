b4ea37b09cc6971aeb3d4f050a9d6fafcf2d9a56
Nitin Jayant <nitinjayant@nojic.com>
2023-05-02 19:32:33 +0000

    redirect to nitinjayant.com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+
+import "./App.css";
+import Welcome from "./app/components/welcome/Welcome";
 
 function App() {
-  return (
-    <div>
-      Hello From Nitin Jayant
-    </div>
-  )
+    return (
+        <div className="App">
+            <Welcome />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/app/components/welcome/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Welcome.css";
+
+export default function Welcome() {
+    return (
+        <div className="welcome-container">
+            You Will Be Redirected to www.NitinJayant.com
+        </div>
+    );
+}
